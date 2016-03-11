4f146b9df04e60734b78bfe0136560fe00ac3928
Chris Maher <chrismaher.wit@gmail.com>
2016-03-11 20:37:49 +0000

    Social Network Password Setup.

--- modules/users/client/controllers/settings/change-password.client.controller.js
+++ modules/users/client/controllers/settings/change-password.client.controller.js
@@ -1,42 +1,40 @@
 'use strict';
 
-angular.module('users').controller('ChangePasswordController', ['$state', '$scope', '$http', 'Authentication',
-  function ($state, $scope, $http, Authentication) {
-    $scope.user = Authentication.user;
-    $scope.authentication = Authentication;
+angular.module('users').controller('ChangePasswordController', ['$window', '$state', '$scope', '$http', 'Authentication',
+    function ($window, $state, $scope, $http, Authentication) {
+        $scope.user = Authentication.user;
+        $scope.authentication = Authentication;
 
-      if ($scope.authentication.user.passwordChanged === 'false') {
+        if ($scope.authentication.user.passwordChanged === 'false') {
 
-          $scope.passwordDetails = {
-              currentPassword: 'changeme'
-          };
+            $scope.passwordDetails = {
+                currentPassword: 'changeme'
+            };
 
-      }
-
-
-  
-
-    // Change user password
-    $scope.changeUserPassword = function (isValid) {
-      $scope.success = $scope.error = null;
+        }
 
 
+        // Change user password
+        $scope.changeUserPassword = function (isValid) {
+            $scope.success = $scope.error = null;
 
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'passwordForm');
 
-        return false;
-      }
+            if (!isValid) {
+                $scope.$broadcast('show-errors-check-validity', 'passwordForm');
 
-      $http.post('/api/users/password', $scope.passwordDetails).success(function (response) {
-        // If successful show success message and clear form
-        $scope.$broadcast('show-errors-reset', 'passwordForm');
-        $scope.success = true;
-        $scope.passwordDetails = null;
-          $state.go('home');
-      }).error(function (response) {
-        $scope.error = response.message;
-      });
-    };
-  }
+                return false;
+            }
+
+            $http.post('/api/users/password', $scope.passwordDetails).success(function (response) {
+                // If successful show success message and clear form
+                $scope.$broadcast('show-errors-reset', 'passwordForm');
+                $scope.success = true;
+                $scope.passwordDetails = null;
+                // And redirect to the previous or home page
+                $state.go($state.previous.state.name || 'home', $state.previous.params);
+            }).error(function (response) {
+                $scope.error = response.message;
+            });
+        };
+    }
 ]);
