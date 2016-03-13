9303399b4ac9fd5efea0b3da241f19fbee242b32
Chris Maher <chrismaher.wit@gmail.com>
2016-03-13 01:25:45 +0000

    Redirect Android

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -2,6 +2,42 @@
 
 angular.module('core').controller('HeaderController', ['$scope', '$location', '$state', 'Authentication', 'Menus',
     function ($scope, $location, $state, Authentication, Menus) {
+
+
+        var isMobile = {
+            Android: function() {
+                return navigator.userAgent.match(/Android/i);
+            },
+            BlackBerry: function() {
+                return navigator.userAgent.match(/BlackBerry/i);
+            },
+            iOS: function() {
+                return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+            },
+            Opera: function() {
+                return navigator.userAgent.match(/Opera Mini/i);
+            },
+            Windows: function() {
+                return navigator.userAgent.match(/IEMobile/i);
+            },
+            any: function() {
+                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+            }
+
+        };
+
+
+
+        if ( isMobile.Android() ) {
+            alert("Android");
+            document.location.href = "http://www.google.ie";
+        }
+        // else if(isMobile.iOS())
+        // {
+        //     document.location.href="x";
+        // }
+
+
         // Expose view variables
         $scope.$state = $state;
         $scope.authentication = Authentication;
--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -7,8 +7,6 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
     // Get an eventual error defined in the URL query string:
     $scope.error = $location.search().err;
 
-
-
     //// If user is signed in then redirect back home
     //if ($scope.authentication.user) {
     //  $location.path('/');
