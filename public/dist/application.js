d8f4f3366aad7755b38fad6eb6537afed93732a7
Chris Maher <chrismaher.wit@gmail.com>
2016-05-02 14:44:37 +0100

    Category Filters.

--- public/dist/application.js
+++ public/dist/application.js
@@ -324,6 +324,7 @@ angular.module('core').controller('HomeController', ['$state','$scope', '$locati
         $scope.numOfCouponsToday = 0;
         
         $scope.selectedLogo = 'All';
+        $scope.selectedCategory = 'All';
         $scope.activeClass = 2;
 
         $scope.hottestsorted2 = false;
@@ -441,9 +442,17 @@ angular.module('core').controller('HomeController', ['$state','$scope', '$locati
 
         };
 
+        $scope.setFilterTextCategory = function (name) {
+
+            $scope.selectedCategory = name;
+
+        };
+
         $scope.setLogo = function (name) {
 
             if(name === 'All'){
+                $scope.brandLogo = name;
+            }else if(name === 'category'){
                 $scope.brandLogo = name;
             }else if(name === 'Littlewoods'){
                 $scope.brandLogo = '/modules/users/client/img/profile/littlewoods-logo.png';
